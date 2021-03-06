fe1bd6045a2e4aefdaefaedece843c469ebdf305
Mateus Oliveira <mateus.oliveira@aluno.ufop.ed.br>
2021-03-06 12:21:58 -0300

    Construtor de Grafo e função que imprime lista de adjacências

--- src/Program.java
+++ src/Program.java
@@ -5,24 +5,85 @@ import java.util.Scanner;
 class Grafo{
 	int numVertices;
 	int numArestas;
-	ArrayList <Integer> listaAdjacencias[];
+	ArrayList <List<Integer>> listaAdjacencias;
 	
-	/*Grafo(int numVertices, int numArestas){ //construtor para iniciar a lista de adjacencia
+	Grafo(int numVertices, int numArestas){ //construtor para iniciar a lista de adjacencia
 		this.numVertices = numVertices;
 		this.numArestas = numArestas;
 		
-		listaAdjacencias[numVertices] = new ArrayList<>(numVertices);
-		for(int i = 0; i <= numVertices; i++) {
-			listaAdjacencias[i] = new ArrayList<>();
+		listaAdjacencias = new ArrayList<>();
+		for(int i = 0; i < numVertices; i++) {
+			listaAdjacencias.add(new ArrayList<>());
 		}
-	}*/
-	
-	int soma() {
-		return numArestas+numVertices;
 	}
 }
 
 
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o numero de arestas: ");
+		int numArestas = sc.nextInt();
+		System.out.println("Digite o numero de vertices: ");
+		int numVertices = sc.nextInt();
+		
+		Grafo a = new Grafo(numVertices, numArestas);
+		
+		addVerticeAdjacente(a.listaAdjacencias, 0, 7);
+		addVerticeAdjacente(a.listaAdjacencias, 0, 2);
+		addVerticeAdjacente(a.listaAdjacencias, 0, 1);
+		addVerticeAdjacente(a.listaAdjacencias, 0, 9);
+		
+		addVerticeAdjacente(a.listaAdjacencias, 1, 9);
+		addVerticeAdjacente(a.listaAdjacencias, 1, 2);
+		addVerticeAdjacente(a.listaAdjacencias, 1, 6);
+		addVerticeAdjacente(a.listaAdjacencias, 1, 5);
+		addVerticeAdjacente(a.listaAdjacencias, 1, 10);
+		
+		imprimeAdjacencias(a.listaAdjacencias);
+		
+		
+		sc.close();
+	}
+	
+	static void addVerticeAdjacente(ArrayList <List<Integer>> listaAdjacencias, int vertice , int novoVerticeAdjacente) { //adiciona um novo vertice adjacente de forma que a lista ja fique ordenada
+		
+		if (listaAdjacencias.get(vertice).isEmpty())
+			listaAdjacencias.get(vertice).add(novoVerticeAdjacente);
+		
+		else {
+			for(int i = 0; i < listaAdjacencias.get(vertice).size(); i++) {
+				if(novoVerticeAdjacente < listaAdjacencias.get(vertice).get(i)) { //verifica se eh menor que um elemento presente na posicao i da lista
+					listaAdjacencias.get(vertice).add(i, novoVerticeAdjacente);
+					return;
+				}
+				else if (i+1 == listaAdjacencias.get(vertice).size()){ //verifica se eh o ultimo elemento da lista, se for adiciona o elemento na ultima posicao
+					listaAdjacencias.get(vertice).add(novoVerticeAdjacente);
+					return;
+				}
+			}
+		}
+	}
+	
+	static void imprimeAdjacencias(ArrayList <List<Integer>> listaAdjacencias) {
+		int v = 1;
+		for(List<Integer> l: listaAdjacencias) {
+			System.out.print("Vertice " + v + ": ");
+			for(int i=0; i<l.size(); i++) {
+				System.out.print("->" + l.get(i) + " ");
+			}
+			v++;
+			System.out.println();
+		}
+	}
+
+}
+
+
+
+/*
 public class Program {
 
 	public static void main(String[] args) {
@@ -34,9 +95,6 @@ public class Program {
 		a.numArestas = sc.nextInt();
 		System.out.println("Digite o numero de vertices: ");
 		a.numVertices = sc.nextInt();
-				
-		System.out.print("Numero de vertices: " + a.numVertices + "\nNumero de arestas: " + a.numArestas + "\n");
-		System.out.println("Soma: " + a.soma());
 		
 		ArrayList <List<Integer>> listaAdjacencias = new ArrayList<>();
 		listaAdjacencias.add(new ArrayList<>());
@@ -78,26 +136,6 @@ public class Program {
 		
 		sc.close();
 	}
-	
-	static void addVerticeAdjacente(ArrayList <List<Integer>> listaAdjacencias, int vertice , int novoVerticeAdjacente) {
-		
-		if (listaAdjacencias.get(vertice).isEmpty())
-			listaAdjacencias.get(vertice).add(novoVerticeAdjacente);
-		
-		else {
-			for(int i = 0; i < listaAdjacencias.get(vertice).size(); i++) {
-				if(novoVerticeAdjacente < listaAdjacencias.get(vertice).get(i)) { //verifica se eh menor que um elemento presente na posicao i da lista
-					listaAdjacencias.get(vertice).add(i, novoVerticeAdjacente);
-					i = listaAdjacencias.get(vertice).size();
-				}
-				else if (i+1 == listaAdjacencias.get(vertice).size()){ //verifica se eh o ultimo elemento da lista, se for adiciona o elemento na ultima posicao
-					listaAdjacencias.get(vertice).add(novoVerticeAdjacente);
-					return;
-				}
-			}
-		}
-	}
-
-}
+*/
 
 
